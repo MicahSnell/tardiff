1c164784081fe1d8e02134f18f776968436fd634
Micah Snell <snellmicah@gmail.com>
2023-02-01 12:38:09 -0600

    improve install script

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-INSTALL_DIR=$(echo $PATH | cut -d ":" -f 1)
-cp -p src/tardiff $INSTALL_DIR
-echo "Copied 'tardiff' to $INSTALL_DIR"
+# get install path
+defaultInstallDir="${HOME}/.local/bin"
+read -p "Enter install path or press enter to install at '${defaultInstallDir}': " \
+     installDir
+installDir="${installDir:-${defaultInstallDir}}"
+
+# verify directory exists
+if [ ! -d "${installDir}" ]; then
+  echo "Failed to install"
+  echo "Directory ${installDir} does not exist"
+  exit 1
+fi
+
+# copy to installDir
+cp src/tardiff "${installDir}"
+if [ "$?" -eq 0 ]; then
+  echo "Copied 'tardiff' to ${installDir}"
+else
+  echo "Failed to copy 'tardiff' to ${installDir}"
+fi
+
